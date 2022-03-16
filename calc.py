76feac345a822753d740e51252b093328bb292a6
Shelf0 <shelffo4@gmail.com>
2022-03-15 21:45:29 -0400

    final test

--- calc.py
+++ calc.py
@@ -1,10 +1,13 @@
-operation = input('''
+#Function Definition
+def calculate():
+    operation = input('''
 Select the math operation you would like to complete:
 + for addition
 - for subtraction
 * for multiplication
 / for division
 ''')
+
 
 #Code for input prompts 
 num_1 = int(input('Enter first number:'))
@@ -32,4 +35,29 @@ elif operation == '/':
 
 else:
     print('Invalid Input, please try again')
+
+again()
+
+#close function 
+calculate()
+
+def again():
+
+    #input 
+    calc_again = input('''
+    Do you want to use the calculator again?
+    Type Y for YES or N for NO.
+    ''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+
+    elif calc_again.upper() == 'N':
+        print('Goodbye.')
+
+    else:
+        again()
+
+calculate()
+
 
