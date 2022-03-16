830cfe929b1f521fc8d5fe2c1ff4ed274dedd501
Shelf0 <shelffo4@gmail.com>
2022-03-15 21:04:40 -0400

    Debugged syntax error

--- calc.py
+++ calc.py
@@ -1,21 +1,35 @@
+operation = input('''
+Select the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
 #Code for input prompts 
 num_1 = int(input('Enter first number:'))
 num_2 = int(input('Enter second number:'))
 
 #Addition
-print('{} + {} ='.format(num_1, num_2))
-print(num_1 + num_2)
+if operation == '+':
+    print('{} + {} ='.format(num_1, num_2))
+    print(num_1 + num_2)
 
 #Subtraction
-print('{} - {} ='.format(num_1, num_2))
-print(num_1 + num_2)
+elif operation == '-':
+    print('{} - {} ='.format(num_1, num_2))
+    print(num_1 - num_2)
 
 #Multiplication
-print('{} * {} ='.format(num_1, num_2))
-print(num_1 + num_2)
+elif operation == '*':
+    print('{} * {} ='.format(num_1, num_2))
+    print(num_1 * num_2)
 
 #Division
-print('{} / {} ='.format(num_1, num_2))
-print(num_1 + num_2)
+elif operation == '/':
+    print('{} / {} ='.format(num_1, num_2))
+    print(num_1 - num_2)
 
+else:
+    print('Invalid Input, please try again')
 
