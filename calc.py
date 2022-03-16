8e9ed25ed4e027c6776eac459f5405478a8d75f3
Shelf0 <shelffo4@gmail.com>
2022-03-15 20:52:06 -0400

    Wrote code for inputs and for 4 basic functions of the calculator

---
+++ calc.py
@@ -0,0 +1,21 @@
+#Code for input prompts 
+num_1 = int(input('Enter first number:'))
+num_2 = int(input('Enter second number:'))
+
+#Addition
+print('{} + {} ='.format(num_1, num_2))
+print(num_1 + num_2)
+
+#Subtraction
+print('{} - {} ='.format(num_1, num_2))
+print(num_1 + num_2)
+
+#Multiplication
+print('{} * {} ='.format(num_1, num_2))
+print(num_1 + num_2)
+
+#Division
+print('{} / {} ='.format(num_1, num_2))
+print(num_1 + num_2)
+
+
